11e026ec9f203105e0b9809bd972fef1b8402700
Tony Wang <g7190305@yahoo-inc.com>
2015-07-26 10:18:04 +0800

    Project_1 init

---
+++ app/src/main/java/com/example/g7190305/instagramclient/InstagramPhoto.java
@@ -0,0 +1,61 @@
+package com.example.g7190305.instagramclient;
+
+/**
+ * Created by g7190305 on 2015/7/25.
+ */
+public class InstagramPhoto {
+    private String username;
+    private String caption;
+    private String imageUrl;
+    private String type;
+    private int imageHeight;
+    private int likesCount;
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getImageHeight() {
+        return imageHeight;
+    }
+
+    public void setImageHeight(int imageHeight) {
+        this.imageHeight = imageHeight;
+    }
+
+    public int getLikesCount() {
+        return likesCount;
+    }
+
+    public void setLikesCount(int likesCount) {
+        this.likesCount = likesCount;
+    }
+}
